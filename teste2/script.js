f8e6fc8fdff0e346eddcec443e2928b115db6966
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-03-13 14:58:29 -0300

    exemplos da aula 1 de JavaScript - DOM e seletores

---
+++ teste2/script.js
@@ -0,0 +1,11 @@
+document.getElementById('page-title').innerText="Os vingadores";
+
+document.getElementById('paragraph').innerText="O melhor filme já produzido pela Marvel";
+
+document.getElementById('subtitle').innerText="Homem de ferro";
+
+document.getElementById('second-paragraph').innerText="Melhor personagem da Marvel";
+
+document.getElementsByClassName('teste1')[0].innerText='O melhor filme de todos os tempos';
+
+document.getElementsByTagName('h4')[0].innerText = 'Thor';
